1debe1b79f1e046597bad92aacd61e4be0b03655
srisail <149557042+BSrisail@users.noreply.github.com>
2026-04-14 12:11:06 +0530

    Enable public IP address association for instances

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = each.value.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
   subnet_id              = var.subnet_id
-
+  associate_public_ip_address = true
   tags = {
     Name        = each.value.name
     Environment = "Development"
